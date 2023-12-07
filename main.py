4162004595d91e3ef4538e63c06f3dcc5ae4083b
y9koto <matveybykov2000@gmail.com>
2023-12-07 17:49:51 +0200

    07.12.2023+1

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print ("Hello")
+class Student:
+    count = 0
+    def __init__(self, name="No Name", height=160):
+        self.height = height
+        self.name = name
+        Student.count += 1
+
+    def print(self):
+        print(f"Я студент {self.name}.")
+        print(f"Мой рост {self.height} см")
+
+
+print(Student.count)
+student = Student("Vitalik",170)
+student.print()
+
+student = Student(height=150)
+student1 = Student(height=180)
+student2 = Student(height=165)
+print(student.height)
+print(student1.height)
+print(student2.height)
+print(student2.count)
